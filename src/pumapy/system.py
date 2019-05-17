2f4340354c7da9ce7a69ad98207002604282d08d
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-05-17 14:43:02 +0200

    Add PpmsSystem class and tests

---
+++ src/pumapy/system.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+"""Module representing bookable systems in PPMS."""
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
+class PpmsSystem(object):
+
+    """Object representing a bookable system in PPMS."""
+
+    def __init__(self, system_id, name, localisation, system_type,
+                 core_facility_ref, schedules, active, stats, bookable,
+                 autonomy_required, autonomy_required_after_hours):
+        """Initialize the system object.
+
+        Parameters
+        ----------
+        system_id : str
+            The system's unique identifier.
+        name : str
+            The system's (human friendly) name.
+        localisation : str
+            The location of the system, corresponds to the "Room" field in the
+            PPMS web interface.
+        system_type : str
+            The "System Type" field in PPMS, e.g. "Confocal Microscopes" or
+            "Virtual Machine class PowerWorkstations"
+        core_facility_ref : str
+            The core facility reference of the system.
+        schedules : bool
+            Flag indicating whether to list this system in the read-only
+            resources and schedules pages of PPMS.
+        active : bool
+            Flag indicating whether the system is active in PPMS.
+        stats : bool
+            Flag indicating whether to integrate this system in the global usage
+            statistics of PPMS or not.
+        bookable : bool
+            Flag indicating whether the system is actually bookable in PPMS. A
+            system that's not bookable can still be used in incident management.
+        autonomy_required : bool
+            Flag indiciating whether explicit permissions are required for a
+            user for being allowed to book the system. Corresponds to the field
+            "User right/training required" in the PPMS web interface.
+        autonomy_required_after_hours : bool
+            Corresponds to the field "User right/training required only after
+            hours" in the PPMS web interface. Unfortunately there is no
+            description given what that actually means - probably it refers to
+            the "after hours" / "non-peak hours".
+        """
+        # TODO: add a constructor dealing with a PUMAPI response
+        self.system_id = system_id
+        self.name = name
+        self.localisation = localisation
+        self.system_type = system_type
+        self.core_facility_ref = core_facility_ref
+        self.schedules = schedules
+        self.active = active
+        self.stats = stats
+        self.bookable = bookable
+        self.autonomy_required = autonomy_required
+        self.autonomy_required_after_hours = autonomy_required_after_hours
+        self.machine_catalogue = None
+        LOG.debug('PpmsSystem initialized: id=%s, name=[%s], localisation=[%s],'
+                  ' system_type=[%s]', system_id, name, localisation,
+                  system_type)
+
+    def __str__(self):
+        return ('system_id: %s, name: %s, localisation: %s, system_type: %s, '
+                'core_facility_ref: %s, schedules: %s, active: %s, '
+                'stats: %s, bookable: %s, autonomy_required: %s, '
+                'autonomy_required_after_hours: %s' %
+                (self.system_id, self.name, self.localisation, self.system_type,
+                 self.core_facility_ref, self.schedules, self.active,
+                 self.stats, self.bookable, self.autonomy_required,
+                 self.autonomy_required_after_hours))
---
+++ tests/test_system.py
@@ -0,0 +1,56 @@
+"""Tests for the PpmsSystem class."""
+
+from pumapy.system import PpmsSystem
+
+__author__ = "Niko Ehrenfeuchter"
+__copyright__ = __author__
+__license__ = "gpl3"
+
+
+FMT_DATE = r'%Y-%m-%d'
+FMT_TIME = r'%H:%M:%S'
+FMT = '%s %s' % (FMT_DATE, FMT_TIME)
+DAY = '2019-05-18'
+TIME_START = '12:30:00'
+TIME_END = '13:15:00'
+START = '%s %s' % (DAY, TIME_START)
+END = '%s %s' % (DAY, TIME_END)
+
+SYS_ID = '42'
+NAME = 'Our Brand-New Microscope'
+LOC = 'The Perfect Microscopy Room'
+SYS_TYPE = 'Seven-Photon Microscopes'
+CORE_REF = '23'
+EXPECTED = ('system_id: %s, name: %s, localisation: %s, system_type: %s, '
+            'core_facility_ref: %s, schedules: True, active: True, '
+            'stats: True, bookable: True, autonomy_required: True, '
+            'autonomy_required_after_hours: False' %
+            (SYS_ID, NAME, LOC, SYS_TYPE, CORE_REF))
+
+
+def create_system():
+    """Helper function to create a PpmsSystem object with default values.
+
+    Returns
+    -------
+    PpmsSystem
+    """
+    return PpmsSystem(
+        system_id=SYS_ID,
+        name=NAME,
+        localisation=LOC,
+        system_type=SYS_TYPE,
+        core_facility_ref=CORE_REF,
+        schedules=True,
+        active=True,
+        stats=True,
+        bookable=True,
+        autonomy_required=True,
+        autonomy_required_after_hours=False
+    )
+
+def test_ppmssystem():
+    """Test the PpmsSystem constructor."""
+    booking = create_system()
+
+    assert booking.__str__() == EXPECTED
