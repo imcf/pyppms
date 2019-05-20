e5ad80677d8f66b764f99cd1dc791c11d68350f6
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-05-20 10:49:29 +0200

    Add example configuration file

---
+++ resources/examples/config.py
@@ -0,0 +1,7 @@
+"""Configuration settings to be imported by pumapy."""
+
+# the URL of the PUMAPI to talk to:
+PUMAPI_URL = 'https://ppms.eu/pythonfacility/pumapi/'
+
+# API key with appropriate permissions to run the desired commands in PPMS:
+PPMS_API_KEY = 'abcdefghijklmnopqrstuvwxyzABCDEF'
