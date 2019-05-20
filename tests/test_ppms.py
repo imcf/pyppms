ce015aebf9ddd2e21e261305a0c2533afc4a71b7
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-05-20 15:26:15 +0200

    Add basic connection tests (requiring a valid API key)

---
+++ tests/test_ppms.py
@@ -0,0 +1,45 @@
+"""Tests for the 'ppms' module."""
+
+# TODO: pylint for Python2 complains about redefining an outer scope when using
+# pytest fixtures, this is supposed to be fixed in newer versions, so it should
+# be checked again after migration to Python3 (see pylint issue #1535):
+# pylint: disable-msg=redefined-outer-name
+
+import logging
+import pytest
+from requests.exceptions import ConnectionError
+
+import pumapyconf
+from pumapy import ppms
+
+__author__ = "Niko Ehrenfeuchter"
+__copyright__ = __author__
+__license__ = "gpl3"
+
+
+@pytest.fixture
+def ppms_connection():
+    """Establish a connection to a PPMS / PUMAPI instance."""
+    conn = ppms.PpmsConnection(pumapyconf.PUMAPI_URL, pumapyconf.PPMS_API_KEY)
+    return conn
+
+def test_ppmsconnection(ppms_connection):
+    """Test establishing a PPMS connection."""
+    assert ppms_connection.status['auth_state'] == 'good'
+
+def test_ppmsconnection_fail(caplog):
+    """Test various ways how establishing a connection could fail."""
+    caplog.set_level(logging.DEBUG)
+
+    # wrong PUMAPI URL:
+    with pytest.raises(ConnectionError):
+        ppms.PpmsConnection('https://url.example', '')
+
+    # no API key:
+    with pytest.raises(ConnectionError):
+        ppms.PpmsConnection(pumapyconf.PUMAPI_URL, '')
+
+    # wrong API key (trailing characters):
+    with pytest.raises(ConnectionError):
+        ppms.PpmsConnection(pumapyconf.PUMAPI_URL,
+                            pumapyconf.PPMS_API_KEY + 'appendixx')
