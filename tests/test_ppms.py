f8cfad7769cd53b4a6506b20f17c5c566ae12203
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-05-20 11:13:10 +0200

    Clean up global variables for ppms test

--- tests/test_ppms.py
+++ tests/test_ppms.py
@@ -9,21 +9,6 @@ __copyright__ = __author__
 __license__ = "gpl3"
 
 
-FMT_DATE = r'%Y-%m-%d'
-FMT_TIME = r'%H:%M:%S'
-FMT = '%s %s' % (FMT_DATE, FMT_TIME)
-DAY = '2019-05-18'
-TIME_START = '12:30:00'
-TIME_END = '13:15:00'
-START = '%s %s' % (DAY, TIME_START)
-END = '%s %s' % (DAY, TIME_END)
-
-USERNAME = 'ppmsuser'
-SYS_ID = '42'
-EXPECTED = 'username: %s - system: %s - ' % (USERNAME, SYS_ID)
-EXPECTED += 'reservation_start: %s - reservation_end: %s'
-
-
 def test_dict_from_single_response():
     """Test the two-line-response-to-dict converter."""
     valid = 'one,two,thr,fou,fiv,six,sev\nasdf,"qwr",true,"true",false,,"false"'
