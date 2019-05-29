c4adeaf80df613ae70717d91cb2b47b285b20efe
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-05-29 14:47:13 +0200

    Update package docstring

--- src/pumapy/__init__.py
+++ src/pumapy/__init__.py
@@ -1,3 +1,6 @@
-"""Python 2 package for talking to Stratocore's PUMAPI."""
+"""The `pumapy` package enables communicating to PUMAPI using Python 2."""
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
+__author__ = "Niko Ehrenfeuchter"
+__copyright__ = __author__
+__license__ = "gpl3"
