3a750afd85aab98437bffd7d34dcefd1711390bb
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-06-05 14:33:22 +0200

    Add simple script to generalize cached responses

---
+++ resources/scripts/filter-examples.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# simple script to filter facility-specific stuff from the cached responses
+
+set -e
+
+DEBUG="true"
+
+debug() {
+    [ -z "$DEBUG" ] && return
+    echo $@
+}
+
+filter_pumapy() {
+    TMPFILE="${1}_tmp"
+    debug "Using tempfile: $TMPFILE"
+    set +e  # the greps below do not match for all files, so they might return non-zero
+    egrep '^(login,id,booked_hours,used_hours,last_res,last_train|Core facility ref,System id,Type,Name,Localisation,Active,Schedules,Stats,Bookable,Autonomy Required,Autonomy Required After Hours)' "$1" > "$TMPFILE"
+    set -e
+    egrep '(pumapy|admin|Python Development System)' "$1" >> "$TMPFILE"
+    mv "$TMPFILE" "$1"
+}
+
+filter_pumapy "tests/cached_responses/stage_0/getadmins/response.txt"
+filter_pumapy "tests/cached_responses/stage_0/getsysrights/id--31.txt"
+filter_pumapy "tests/cached_responses/stage_0/getgroups/response.txt"
+filter_pumapy "tests/cached_responses/stage_0/getuserexp/response.txt"
+filter_pumapy "tests/cached_responses/stage_0/getusers/active--true.txt"
+filter_pumapy "tests/cached_responses/stage_0/getusers/response.txt"
+filter_pumapy "tests/cached_responses/stage_0/getsystems/response.txt"
+filter_pumapy "tests/cached_responses/stage_1/getsysrights/id--31.txt"
+filter_pumapy "tests/cached_responses/stage_2/getsysrights/id--31.txt"
