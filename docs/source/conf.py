88efc5a289dcc30846ae727404a2475fcc57afb6
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-06-11 09:25:52 +0200

    Remove hard-coded release / version from sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = u'Niko Ehrenfeuchter'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0.0'
+release = u''
 
 
 # -- General configuration ---------------------------------------------------
