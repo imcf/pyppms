243ac949e6d06c023fc2c0d3f321c040014e56fc
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-04-01 22:13:48 +0200

    Clean up __init__.py
    Package metadata goes into the pyproject.toml file.

--- src/pumapy/__init__.py
+++ src/pumapy/__init__.py
@@ -5,12 +5,4 @@
 
 __version__ = "1.1.0"
 
-__copyright__ = "Copyright 2018-2022, Imaging Core Facility, University of Basel"
-__license__ = "GPL3"
-
-__author__ = "Niko Ehrenfeuchter"
-__maintainer__ = __author__
-__email__ = "nikolaus.ehrenfeuchter@unibas.ch"
-__credits__ = [__author__]
-
 from .ppms import PpmsConnection
