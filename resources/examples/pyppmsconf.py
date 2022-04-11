f95c43c67cf328c1d7656dbca5b7ab82528ec54a
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-04-11 16:57:31 +0200

    Formatting

--- resources/examples/pyppmsconf.py
+++ resources/examples/pyppmsconf.py
@@ -1,10 +1,10 @@
 """Configuration settings to be imported by pyppms."""
 
 # the URL of the PUMAPI to talk to:
-PUMAPI_URL = 'https://ppms.eu/pythonfacility/pumapi/'
+PUMAPI_URL = "https://ppms.eu/pythonfacility/pumapi/"
 
 # API key with appropriate permissions to run the desired commands in PPMS:
-PPMS_API_KEY = 'abcdefghijklmnopqrstuvwxyzABCDEF'
+PPMS_API_KEY = "abcdefghijklmnopqrstuvwxyzABCDEF"
 
 # requests timeout in seconds (default=10)
 TIMEOUT = 10
@@ -15,4 +15,4 @@ CACHE_PATH = "tests/cached_responses"
 
 # TESTING ONLY: path to mocked responses (either relative to the repository root or an
 # absolute path)
-MOCKS_PATH = 'tests/mocked_responses'
+MOCKS_PATH = "tests/mocked_responses"
