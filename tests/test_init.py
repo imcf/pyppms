d96a09d38efcadab721d1580b4c70e7b3ae32c08
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-04-13 11:54:19 +0200

    Add test for package version

---
+++ tests/test_init.py
@@ -0,0 +1,8 @@
+"""Tests for the package's __init__ file."""
+
+from pyppms import __version__
+
+
+def test_version():
+    """Ensure we're running against the expected package version."""
+    assert __version__ == "1.1.0-dev0"
