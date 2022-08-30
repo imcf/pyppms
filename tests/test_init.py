724d7be246ccae58b57e855d7f9e595aaf37929f
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-08-30 09:23:36 +0200

    Release pyppms-2.1.0

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,4 +5,4 @@ from pyppms import __version__
 
 def test_version():
     """Ensure we're running against the expected package version."""
-    assert __version__ == "2.1.0-dev0"
+    assert __version__ == "2.1.0"
