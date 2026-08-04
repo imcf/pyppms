2c2e47f2dbd171b4249fdaee1fde4bda65305132
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2026-08-05 00:02:57 +0200

    🚧 Add FIXME

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -161,6 +161,10 @@ def test_booking_from_request():
         PpmsBooking(23, "next", SYS_ID)
 
 
+# FIXME! Create a test for bookings ending at midnight!
+# cf. PpmsBooking.endtime_fromstr()
+
+
 def test_runningsheet(
     runningsheet_response,
     user_details_raw,
