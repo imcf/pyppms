1b41dae17061714b624be318f54005b7d243e59c
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2026-08-08 23:00:05 +0200

    🧫 Update tests to recent changes

--- tests/test_user.py
+++ tests/test_user.py
@@ -22,11 +22,11 @@ def test_user_billing_info(user_details, ppms_connection):
     user = PpmsUser(user_details["api_response"], conn=ppms_connection)
     assert len(user.billing_info) == 3
 
-    assert user.billing_info[0].billing_code == "pyppms_group_billing_code"
-    assert user.billing_info[0].billing_type == "group"
+    assert user.billing_info[0].billing_code == "pyppms_user_billing_code"
+    assert user.billing_info[0].billing_type == "user"
 
-    assert user.billing_info[1].billing_code == "pyppms_user_billing_code"
-    assert user.billing_info[1].billing_type == "user"
+    assert user.billing_info[1].billing_code == "pyppms_group_billing_code"
+    assert user.billing_info[1].billing_type == "group"
 
     assert user.billing_info[2].billing_code == "proj.bcode.6"
     assert user.billing_info[2].billing_type == "project"
